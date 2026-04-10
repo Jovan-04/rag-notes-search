b393918bb1de5d96c53fa29e9017d75ec7389e9c
Evan <evanne.scherrer@gmail.com>
2026-04-09 19:55:14 -0500

    add maybe functioning LLM promptinggit statusgit status

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Rag Notes Search
 more to come (including a better name)
+
+
+## Backend Endpoints
+`/prompt`
+* prompts a language model to use RAG to answer a question
+* single required query parameter, `q` with the question for the language model
+* returns the language model's response after it's done generating
+* e.g. `/prompt?q=paranoia`
+
+`/search`
+* uses semantic search to return relevant sections of student notes
+* single required query parameter, `q` with the search query
+* returns JSON; each result dict contains `match_number`, `document_text`, and `source_file` fields
+```json
+{
+    query: str,
+    results: list[dict]
+}
+```
+* e.g. `/prompt?q=paranoia`
--- prompt.py
+++ prompt.py
@@ -1,2 +1,41 @@
-def prompt(query: str):
-    pass
+from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
+
+MODEL_NAME = 'Qwen/Qwen2.5-1.5B-Instruct'
+chatbot = pipeline('text-generation', model=MODEL_NAME, dtype='auto', device_map='auto')
+# tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+# model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, torch_dtype='auto', device_map='auto')
+
+def prompt(query: str) -> str:
+    with open('/home/evan/evan-drake/cs-195/creative-synthesis/rag-notes-search/Notes/world_music/India.md') as notes:
+        retrieved = notes.read()
+    
+    query = "what is raga?"
+
+    print(f"received query {query}, generating response...")
+
+    prompt = [
+        {
+            'role': 'system',
+            'content': (
+                'You are a study partner for a student. '
+                "Answer using only the retrieved information from the student's notes when possible. "
+                'If the answer is not supported by the retrieved context, say that you do not know based on the provided notes. '
+            ),
+        },
+        {
+            'role': 'user',
+            'content': (
+                f"Query: {query}\n\n"
+                f"Retrieved course information:\n{retrieved}\n\n"
+                'Please answer the question using the retrieved notes information.'
+            ),
+        },
+    ]
+
+    # text = tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=False)
+
+    # streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+    # model_inputs = tokenizer([text])
+
+    rag_response = chatbot(prompt)
+    return rag_response[0]['generated_text'][-1]['content']
